063d923bd5909e7e5050fd9645c756c8ca171f5a
Vipul Gaur <gaurvipul@gmail.com>
2019-09-08 06:50:54 +0530

    Text Mining

---
+++ Day-9/assisted_practice/ner_bin_false.py
@@ -0,0 +1,25 @@
+# Run with Terminal
+
+import nltk
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
+tokenized = custom_sent_tokenizer.tokenize(sample_text)
+
+def process_content():
+    try:
+        for i in tokenized[5:]:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            namedEnt = nltk.ne_chunk(tagged, binary=False)
+            namedEnt.draw()
+    except Exception as e:
+        print(str(e))
+
+
+process_content()
